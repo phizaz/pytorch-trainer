9fb1878df7c63edd52e45bdbe311cc1a4487d667
Konpat Preechakul <the.akita.ta@gmail.com>
2020-11-21 12:36:21 +0700

    update

--- trainer/dataset.py
+++ trainer/dataset.py
@@ -1,10 +1,13 @@
-from mlkit.start import *
+from torch.utils.data import Dataset
+
 
 class SubsetDataset(Dataset):
     def __init__(self, original_dataset, size):
         self.original_dataset = original_dataset
         self.size = size
-        assert len(self.original_dataset) >= size, f'the subset size must be smaller than the original size.'
+        assert len(
+            self.original_dataset
+        ) >= size, f'the subset size must be smaller than the original size.'
 
     def __len__(self):
         # resize the dataset
