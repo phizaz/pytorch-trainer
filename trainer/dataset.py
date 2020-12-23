5e50c33a49288b5fe59bcfd74896d7b53e326cb9
root <root@U11-ta.eng.chula.ac.th>
2020-12-24 01:35:14 +0700

    update

--- trainer/dataset.py
+++ trainer/dataset.py
@@ -4,10 +4,7 @@ from torch.utils.data import Dataset
 class SubsetDataset(Dataset):
     def __init__(self, original_dataset, size):
         self.original_dataset = original_dataset
-        self.size = size
-        assert len(
-            self.original_dataset
-        ) >= size, f'the subset size must be smaller than the original size.'
+        self.size = min(size, len(original_dataset))
 
     def __len__(self):
         # resize the dataset
