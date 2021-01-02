eaa1b617c97f54b841689d1b6b148ef108c497e0
Konpat Preechakul <the.akita.ta@gmail.com>
2021-01-03 01:43:01 +0700

    update

--- trainer/callbacks/profiler_cb.py
+++ trainer/callbacks/profiler_cb.py
@@ -5,14 +5,20 @@ from .base_cb import *
 
 _BUCKET = {}
 
+
 def put_to_profiler(d: Dict):
     """update values to the bucket"""
     if d['i_itr'] is None:
         del d['i_itr']
     _BUCKET.update(d)
 
+
 @contextmanager
-def time_elapsed_to_profiler(key, i_itr=None, enable=True, prefix='profiler/'):
+def time_elapsed_to_profiler(key,
+                             i_itr=None,
+                             enable=True,
+                             prefix='profiler/',
+                             verbose=False):
     """a contextmanager that logs the time of the block
     Args:
         i_itr: only required once per iteration
@@ -20,11 +26,15 @@ def time_elapsed_to_profiler(key, i_itr=None, enable=True, prefix='profiler/'):
     begin_time = time.time()
     yield
     end_time = time.time()
+    elapsed = end_time - begin_time
     if enable:
         put_to_profiler({
             'i_itr': i_itr,
-            f'{prefix}{key}': end_time - begin_time,
+            f'{prefix}{key}': elapsed,
         })
+        if verbose:
+            print(f'{prefix}{key}:', elapsed)
+
 
 class ProfilerCb(BoardCallback):
     """
